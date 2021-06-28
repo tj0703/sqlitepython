03319764b7509cc6b6e3ff438c1adc6b48ebb6d5
tj0703 <tanvi.jain@aalto.fi>
2021-06-28 13:06:37 +0300

    insert data in database

--- main.py
+++ main.py
@@ -11,10 +11,41 @@ def create_connection(db_file):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        return conn
     except Error as e:
         print(e)
     return conn
+
+
+def create_project(conn, project):
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param project:
+    :return: project id
+    """
+    sql = ''' INSERT INTO projects(name,begin_date,end_date)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_task(conn, task):
+    """
+    Create a new task
+    :param conn:
+    :param task:
+    :return: task id
+    """
+
+    sql = ''' INSERT INTO tasks(name,priority,status_id,project_id,begin_date,end_date)
+              VALUES(?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+
+    return cur.lastrowid
 
 
 def create_table(conn, create_table_sql):
@@ -33,36 +64,54 @@ def create_table(conn, create_table_sql):
 def main():
     database = r"/Users/Tanvi/sqlite/db/pythonsqlite.db"
 
-    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
-                                        id integer PRIMARY KEY,
-                                        name text NOT NULL,
-                                        begin_date text,
-                                        end_date text
-                                    ); """
-
-    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
-                                    id integer PRIMARY KEY,
-                                    name text NOT NULL,
-                                    priority integer,
-                                    status_id integer NOT NULL,
-                                    project_id integer NOT NULL,
-                                    begin_date text NOT NULL,
-                                    end_date text NOT NULL,
-                                    FOREIGN KEY (project_id) REFERENCES projects (id)
-                                );"""
-
     # create a database connection
     conn = create_connection(database)
 
-    # create tables
-    if conn is not None:
-        # create projects table
-        create_table(conn, sql_create_projects_table)
+    # uncomment the below code while creating tables.
+    # sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+    #                                     id integer PRIMARY KEY,
+    #                                     name text NOT NULL,
+    #                                     begin_date text,
+    #                                     end_date text
+    #                                 ); """
+    #
+    # sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+    #                                 id integer PRIMARY KEY,
+    #                                 name text NOT NULL,
+    #                                 priority integer,
+    #                                 status_id integer NOT NULL,
+    #                                 project_id integer NOT NULL,
+    #                                 begin_date text NOT NULL,
+    #                                 end_date text NOT NULL,
+    #                                 FOREIGN KEY (project_id) REFERENCES projects (id)
+    #                             );"""
+    #
+    # # create a database connection
+    # conn = create_connection(database)
+    #
+    # # create tables
+    # if conn is not None:
+    #     # create projects table
+    #     create_table(conn, sql_create_projects_table)
+    #
+    #     # create tasks table
+    #     create_table(conn, sql_create_tasks_table)
+    # else:
+    #     print("Error! cannot create the database connection.")
 
-        # create tasks table
-        create_table(conn, sql_create_tasks_table)
-    else:
-        print("Error! cannot create the database connection.")
+    # use the below code to insert data
+    with conn:
+        # create a new project
+        project = ('Cool App with SQLite & Python', '2015-01-01', '2015-01-30');
+        project_id = create_project(conn, project)
+
+        # tasks
+        task_1 = ('Analyze the requirements of the app', 1, 1, project_id, '2015-01-01', '2015-01-02')
+        task_2 = ('Confirm with user about the top requirements', 1, 1, project_id, '2015-01-03', '2015-01-05')
+
+        # create tasks
+        create_task(conn, task_1)
+        create_task(conn, task_2)
 
 
 if __name__ == '__main__':
